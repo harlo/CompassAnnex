83ef84014392bcc684ba79f09a7e622a4873caa3
harlo <harlo.holmes@nytimes.com>
2014-06-09 16:11:56 +0000

    better stubs for nlp

--- Models/cp_nlp_server.py
+++ Models/cp_nlp_server.py
@@ -6,19 +6,23 @@ from lib.Core.Utils.funcs import stopDaemon, startDaemon
 from Utils.funcs import printAsLog
 from conf import MONITOR_ROOT, DEBUG, getConfig
 
-class CompassNLPServerHandler(object):
-	def __init__(self):
-		if DEBUG: print "initing nlp handler"
-
-class CompassNLPServer(CompassNLPServerHandler):
+class CompassNLPServer(object):
 	def __init__(self):
 		if DEBUG: print "initing Stanford Core NLP Server"
+		
 		self.pid_file = os.path.join(MONITOR_ROOT, "nlp_svr.pid.txt")
 		self.log_file = os.path.join(MONITOR_ROOT, "nlp_svr.log.txt")
 		self.status_file = os.path.join(MONITOR_ROOT, "nlp_svr.status.txt")
-		
-		CompassNLPServerHandler.__init__(self)
 	
+	def tokenize(self, nlp_server, texts, blocking=True):
+		if type(texts) is not list: texts = [texts]
+		
+		for text in texts: nlp_server.send(text)
+		
+		if blocking: return nlp_server.getAll()
+		else: return True
+		# TODO: fail states.
+			
 	def startServer(self):
 		self.svr_path = getConfig('nlp_server.path')
 		self.svr_port = getConfig('nlp_server.port')
@@ -44,3 +48,18 @@ class CompassNLPServer(CompassNLPServerHandler):
 		
 		stopDaemon(self.pid_file, extra_pids_port=self.svr_port)
 		with open(self.status_file, 'wb+') as status_file: status_file.write("False")
+		
+		try:
+			del self.nlp_server
+		except Exception as e:
+			print "error stopping NLP server\n%s" % e
+	
+	def sendNLPRequest(self, query):
+		if not hasattr(self, "nlp_server"):
+			if DEBUG: print "starting NLP server before requesting"
+			self.startServer()
+			sleep(5)
+		
+		# TODO: if server is unusable		
+		return None
+			
