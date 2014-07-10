c28eda3b7d292f176547cdd298557d0ac85b42c0
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2014-07-10 17:55:07 -0400

    adds untested address parser using Everyblock's address extractor

--- Tasks/NLP/address_parser.py
+++ Tasks/NLP/address_parser.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
 
+from addresses import parse_addresses
+
 from vars import CELERY_STUB as celery_app
 
 @celery_app.task
-def basicTokenizer(task):
+def addressParser(task):
 	task_tag = "NLP ADDRESS PARSER"
 	print "\n\n************** %s [START] ******************\n" % task_tag
-	print "TOKENIZING TEXT DOCUMENT at %s" % task.doc_id
+	print "EXTRACTING ADDRESSES FROM TEXT DOCUMENT at %s" % task.doc_id
 	task.setStatus(412)
 
 	from lib.Worker.Models.uv_document import UnveillanceDocument
@@ -36,4 +38,26 @@ def basicTokenizer(task):
 		print "\n\n************** %s [ERROR] ******************\n" % task_tag
 		return
 	
+	addresses = parse_addresses(txt)
 	
+	if addresses is None:
+		print "COULD NOT EXTRACT ADDRESSES."
+		print "\n\n************** %s [ERROR] ******************\n" % task_tag
+		return
+	
+	if DEBUG:
+		print "here is res"
+		print type(addresses)
+		
+	asset_path = doc.addAsset(addresses, "addresses.json", as_literal=False,
+		description="addresses output from Everyblock address extractor",
+		tags=[ASSET_TAGS['ADDRESSES_NLP']])
+
+	if asset_path is None or not doc.addFile(asset_path, None, sync=True): 
+		print "COULD NOT SAVE ASSET."
+		print "\n\n************** %s [ERROR] ******************\n" % task_tag
+		return
+	
+	doc.addCompletedTask(task.task_path)
+	task.finish()
+	print "\n\n************** %s [END] ******************\n" % task_tag
