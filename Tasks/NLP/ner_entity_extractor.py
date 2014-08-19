eaa1883700ca7a7bfd5523dba27cffd8a4f4b522
harlo <harlo.holmes@gmail.com>
2014-08-19 14:20:05 +0000

    NER-tagger complete

--- Tasks/NLP/ner_entity_extractor.py
+++ Tasks/NLP/ner_entity_extractor.py
@@ -29,9 +29,10 @@ def extractNEREntities(task):
 		print "\n\n************** %s [ERROR] ******************\n" % task_tag
 		return
 
+	import os
 	from nltk.tag.stanford import NERTagger
 	from conf import getConfig
-	from lib.Core.Utils.funcs import cleanAndSplitLine
+	from lib.Core.Utils.funcs import cleanLine
 
 	st = NERTagger(
 		os.path.join(getConfig('nlp_ner_base'), 
@@ -40,40 +41,50 @@ def extractNEREntities(task):
 
 	entities = {}
 
-	for sentence in [cleanAndSplitLine(text) for text in texts if text is not None]:
-		lemmas = st.tag(sentence)
+	pages = [text for text in texts]
+	for i, page in enumerate(pages):
+		if page is None: continue
+
+		print "LEMMATIZING PAGE %d of %d" % (i, len(pages))
+		try:
+			lemmas = st.tag(cleanLine(page).split())
+		except Exception as e:
+			print "\n\n************** %s [WARN] ******************\n" % task_tag
+			print e
+			continue
+
 		if len(lemmas) == 0: continue
 
-		last_pos = "O"
+		if DEBUG: print "page has %d lemmas" % len(lemmas)
+
 		index = 0
 		current_entity = []
+		last_pos = "O"
 
-		while last_pos is not None:
+		while True:
 			try:
 				entity = lemmas[index]
 			except Exception as e:
-				last_pos = None
+				entities = updateEntities(entities, current_entity, last_pos, i)				
 				break
 
 			if entity[1] not in ["O"]:
+				print entity, last_pos
+
 				if entity[1] != last_pos:
-					if(len(current_entity) > 0):
-						if entity[1].lower() not in entities.keys():
-							entities[entity[1].lower()] = []
+					entities = updateEntities(entities, current_entity, entity[1], i)
+					current_entity = []
 
-						entities[entity[1].lower()].push(" ".join(current_entity))
-						current_entity = []
-
-				current_entity.push(entity[0])
+				current_entity.append(entity[0])
 			else:
-				if(len(current_entity) > 0): current_entity = []
-
+				entities = updateEntities(entities, current_entity, last_pos, i)
+				current_entity = []
 
 			last_pos = entity[1]
 			index += 1
 
 	if DEBUG: print entities
-	if len(entities.keys() > 0):
+	if len(entities.keys()) > 0:
 		ner_entity_path = doc.addAsset(entities, "stanford-ner_entities.json", as_literal=False,
 			description="Entities as per Stanford-NER Tagger (via NLTK)",
 			tags=[ASSET_TAGS['STANFORD_NER_ENTITIES'], ASSET_TAGS['CP_ENTITIES']])
@@ -85,3 +96,36 @@ def extractNEREntities(task):
 	task.routeNext()
 	task.finish()
 	print "\n\n************** %s [END] ******************\n" % task_tag
+
+def updateEntities(entities, current_entity, last_pos, page):
+	from conf import DEBUG
+
+	if len(current_entity) != 0:
+		entity = " ".join(current_entity)
+
+		if last_pos.lower() not in entities.keys():
+			entities[last_pos.lower()] = []
+		
+		if entity not in entities[last_pos.lower()]:
+			entities[last_pos.lower()].append(entity)
+
+		if DEBUG: 
+			print "ENTITY FOUND: %s" % entity
+
+		if "uv_page_map" not in entities.keys():
+			entities["uv_page_map"] = []
+
+		in_page_map = [e for e in entities['uv_page_map'] if e['entity'] == entity]
+		if len(in_page_map) != 1:
+			entities['uv_page_map'].append({
+				'entity' : entity,
+				'pages' : [page],
+				'count' : 1
+			})
+		else:
+			if page not in in_page_map[0]['pages']: in_page_map[0]['pages'].append(page)
+			in_page_map[0]['count'] += 1
+
+		
+
+	return entities
