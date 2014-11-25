6f07c07309762e85579c7e5b6878145a20e979d1
harlo <harlo.holmes@gmail.com>
2014-11-25 04:34:05 -0500

    a quick-and-dirty ocr job

--- Tasks/PDF/ocr_pdf.py
+++ Tasks/PDF/ocr_pdf.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import
 from vars import CELERY_STUB as celery_app
 
 @celery_app.task
-def OCRPDF(task):	
+def OCRPDF(uv_task):	
 	task_tag = "PDF OCR-TO-TEXT"
 	print "\n\n************** %s [START] ******************\n" % task_tag
-	print "OCRing text from pdf at %s" % task.doc_id
+	print "OCRing text from pdf at %s" % uv_task.doc_id
 	task.setStatus(302)
 
 	from lib.Worker.Models.cp_pdf import CompassPDF
@@ -14,11 +14,11 @@ def OCRPDF(task):
 	from conf import DEBUG
 	from vars import ASSET_TAGS
 
-	pdf = CompassPDF(_id=task.doc_id)
+	pdf = CompassPDF(_id=uv_task.doc_id)
 	if pdf is None:
 		print "PDF IS NONE"
 		print "\n\n************** %s [ERROR] ******************\n" % task_tag
-		task.fail()
+		uv_task.fail()
 		return
 
 	"""
@@ -27,29 +27,68 @@ def OCRPDF(task):
 		
 	"""
 	pdf_reader = pdf.loadFile(pdf.file_name)
-	total_pages = pdf_reader.getNumPages()
-	if hasattr(task, "split_file"):
-		pdf_reader = pdf.loadAsset(task.split_file)		
-
 	if pdf_reader is None:
 		print "PDF READER IS NONE"
 		print "\n\n************** %s [ERROR] ******************\n" % task_tag
-		task.fail()
+		uv_task.fail()
 		return
 
-	lower_bound = 0
-	upper_bound = lower_bound + pdf_reader.getNumPages()
-	texts = [None] * total_pages
+	from fabric.api import settings, local
+	from wand.image import Image
+	from time import sleep
 
-	for x in xrange(lower_bound, upper_bound):
-		# TODO: OCR the doc
-		texts[x] = "TBD"
+	from lib.Core.Utils.funcs import cleanLine
+	from Models.uv_els_stub import UnveillanceELSStub
+
+	texts = [None] * pdf.total_pages
 	
-	asset_path = pdf.addAsset(texts, "doc_ocr.json", as_literal=False,
-		description="jsonified texts in document; page-by-page.  From OCR",
-		tags=[ASSET_TAGS['TXT_OCR']])
-	if asset_path is not None: pdf.addFile(asset_path, None, sync=True)
+	if pdf.hasParts():
+		extractors = pdf.getParts()
+	else:
+		extractors = [pdf.file_name]
 	
-	pdf.addCompletedTask(task.task_path)
+	count = 0
+	for e in extractors:
+		if e == pdf.file_name:
+			pdf_reader = pdf.loadFile(e)
+		else:
+			pdf_reader = pdf.loadAsset(e)
+		try:
+			num_pages = pdf_reader.getNumPages()
+		except AttributeError as e:
+			print e
+			continue
+
+		for x in xrange(0, num_pages):
+			# pdf page to image
+			# image to ocr
+			with Image(filename="%s[%d]" % ()) as p_image:
+				p_image.save(os.path.join(ANNEX_DIR, pdf.base_path, "p_image.jpg"))
+				
+				with settings(warn_only=True):
+					text = cleanLine(local("tesseract p_image.jpg -", capture=True))
+					texts[count] = text
+
+					els_stub = UnveillanceELSStub('cp_page_text', inflate={
+						'media_id' : pdf._id,
+						'searchable_text' : text,
+						'index_in_parent' : count
+					})
+
+			count += 1
+
+	asset_path = pdf.addAsset(texts, "doc_texts.json", as_literal=False,
+		description="jsonified texts in document; page-by-page, segment-by-segment. unclean. (OCR'd using tesseract)",
+		tags=[ASSET_TAGS['TXT_JSON']])
+
+	if asset_path is not None: 
+		pdf.addFile(asset_path, None, sync=True)
+		pdf.save()
+
+	del texts
+
+	pdf.addCompletedTask(uv_task.task_path)
+	uv_task.routeNext(inflate={ 'text_file' : asset_path })
 	print "\n\n************** %s [END] ******************\n" % task_tag
-	task.finish()
+
+	uv_task.finish()
